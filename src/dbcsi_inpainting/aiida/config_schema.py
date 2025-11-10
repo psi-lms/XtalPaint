b09813dc2f14e412a9807f599dd85ca9b21baf67
t-reents <timo.reents1@gmail.com>
2025-11-10 16:00:01 +0100

    Update pydantic configurations

--- src/dbcsi_inpainting/aiida/config_schema.py
+++ src/dbcsi_inpainting/aiida/config_schema.py
@@ -9,6 +9,7 @@ from typing import Optional
 from dbcsi_inpainting.aiida.data import (
     BatchedStructures,
     InpaintingStructure,
+    BatchedStructuresData,
 )
 from aiida import orm
 from ase import Atoms
@@ -23,14 +24,24 @@ class RelaxParameters(BaseModel):
     """:param load_path: Path to the model checkpoint for relaxation."""
     fmax: float = 0.05
     """:param fmax: Maximum force for relaxation."""
-    fix_elements: Optional[list[str]] = Field(
-        default=None, description="List of elements to fix during relaxation."
+    elements_to_relax: Optional[list[str]] = Field(
+        default=None,
+        description="List of elements to relax during optimization.",
     )
-    """:param fix_elements: Elements to fix during relaxation."""
+    """:param elements_to_relax: Elements to relax during optimization."""
+    max_natoms_per_batch: int = 512
+    """:param max_natoms_per_batch: Maximum number of atoms per batch for relaxation."""
+    max_n_steps: int = 500
+    """:param max_n_steps: Maximum number of steps for relaxation."""
     device: str = "cpu"
     """:param device: Device to run relaxation on."""
     filter: Optional[str] = None
     """:param filter: Filter to apply during relaxation."""
+    optimizer: str
+    mlip: str
+    return_initial_energies: bool = False
+    return_initial_forces: bool = False
+    return_final_forces: bool = False
 
 
 class InpaintingModelParams(BaseModel):
@@ -54,7 +65,7 @@ class InpaintingModelParams(BaseModel):
     batch_size: int
     N_samples_per_structure: int
     n_resample_steps: Optional[int] = None
-    jump_length: Optional[float] = None
+    jump_length: Optional[int] = None
 
 
 class InpaintingPipelineParams(BaseModel):
@@ -137,11 +148,27 @@ class GenInpaintingCandidatesParams(BaseModel):
             per structure.
         element (str | dict[str, str]): Element(s) to inpaint.
         num_samples (int): Number of samples to generate per region.
+        code_label (Optional[str]): Optional label for the code.
     """
 
     n_inp: int | dict[str, int]
     element: str | dict[str, str]
     num_samples: int = 1
+    # code_label: Optional[str]
+
+
+class EvalParameters(BaseModel):
+    """Parameters for evaluation of inpainting results.
+
+    Attributes:
+        max_workers (int): Maximum number of workers for parallel evaluation.
+        code_label (Optional[str]): Optional label for the code.
+    """
+
+    max_workers: int = 6
+    chunksize: int = 50
+    metrics: str | list[str] = "match"
+    code_label: Optional[str] = None
 
 
 class InpaintingWorkGraphConfig(BaseModel):
@@ -157,6 +184,8 @@ class InpaintingWorkGraphConfig(BaseModel):
             Optional[GenInpaintingCandidatesParams]]
             ): Parameters for generating inpainting candidates.
         code_label (Optional[str]): Optional label for the code plugin.
+        relax_code_label (Optional[str]): Optional label for the relaxation code.
+        inpainting_code_label (Optional[str]): Optional label for the inpainting code.
         relax (Optional[bool]): Whether to perform a relaxation step after
             inpainting.
         relax_kwargs (Optional[RelaxKwargs]): Keyword arguments for the
@@ -164,21 +193,38 @@ class InpaintingWorkGraphConfig(BaseModel):
         full_relax (Optional[bool]): Whether to perform a full relaxation
             ignoring cell constraints.
         options (Optional[dict]): Additional execution options.
+        relax_options (Optional[dict]): Options for the relaxation step.
+        gen_inpainting_candidates_options (Optional[dict]): Options for
+            generating inpainting candidates.
+        inpainting_pipeline_options (Optional[dict]): Options for the
+            inpainting pipeline.
+        evaluate (Optional[bool]): Whether to evaluate the inpainting results.
+        evaluate_params (Optional[EvalParameters]): Parameters for evaluation.
     """
 
-    structures: dict[str, Structure | InpaintingStructure] | BatchedStructures
+    structures: (
+        dict[str, Structure | InpaintingStructure]
+        | BatchedStructures
+        | BatchedStructuresData
+    )
+    run_inpainting: bool = True
     inpainting_pipeline_params: InpaintingPipelineParams
     gen_inpainting_candidates_params: Optional[
         GenInpaintingCandidatesParams
     ] = None
     code_label: Optional[str] = None
+    relax_code_label: Optional[str] = None
+    inpainting_code_label: Optional[str] = None
     relax: Optional[bool] = False
     relax_kwargs: Optional[RelaxParameters] = {}
     full_relax: Optional[bool] = False
+    full_relax_wo_pre_relax: Optional[bool] = False
     options: Optional[dict] = {}
     relax_options: Optional[dict] = {}
     gen_inpainting_candidates_options: Optional[dict] = {}
     inpainting_pipeline_options: Optional[dict] = {}
+    evaluate: Optional[bool] = False
+    evaluate_params: Optional[EvalParameters] = None
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
@@ -193,25 +239,28 @@ class InpaintingWorkGraphConfig(BaseModel):
         Raises:
             TypeError: If the structure mapping is not valid.
         """
-        if not isinstance(v, dict):
+        structures = v
+        if isinstance(v, (BatchedStructures, BatchedStructuresData)):
+            structures = v.get_structures(strct_type="pymatgen")
+        elif not isinstance(v, dict):
             raise TypeError(
                 "Expected a dictionary of StructureData objects, "
                 f"got {type(v)}"
             )
-        if not all(isinstance(k, str) for k in v.keys()):
+        if not all(isinstance(k, str) for k in structures.keys()):
             raise TypeError("All keys in the dictionary must be strings")
         if not all(
             isinstance(
                 s, (orm.StructureData, Structure, Atoms, InpaintingStructure)
             )
-            for s in v.values()
+            for s in structures.values()
         ):
             raise TypeError(
                 "All values in the dictionary must be of type StructureData, "
                 "Structure, ase.Atoms, or InpaintingStructure"
             )
 
-        types = {type(s) for s in v.values()}
+        types = {type(s) for s in structures.values()}
         if len(types) > 1:
             raise TypeError(
                 "All values in the dictionary must be of the same type"
@@ -226,7 +275,11 @@ class InpaintingWorkGraphConfig(BaseModel):
         Ensure that 'gen_inpainting_candidates_params' is provided when
         structures are not already InpaintingStructure instances.
         """
-        values = list(cfg.structures.values())
+        values = (
+            list(cfg.structures.values())
+            if isinstance(cfg.structures, dict)
+            else cfg.structures.get_structures(strct_type="pymatgen")
+        )
         if not all(isinstance(s, InpaintingStructure) for s in values):
             if cfg.gen_inpainting_candidates_params is None:
                 raise ValueError(
@@ -235,10 +288,24 @@ class InpaintingWorkGraphConfig(BaseModel):
                 )
         return cfg
 
+    @model_validator(mode="after")
+    @classmethod
+    def check_evaluate_inpainting_structures(cls, cfg):
+        """Check if structures are already InpaintingStructure objects."""
+        if cfg.evaluate and cfg.is_inpainting_structures:
+            raise ValueError(
+                "If 'evaluate' is True, structures must not be "
+                "InpaintingStructure objects. We need the original structures "
+                "to compare against inpainted structures."
+            )
+        return cfg
+
     @property
     def is_inpainting_structures(self) -> bool:
         """Check if structures are already InpaintingStructure objects."""
-        return all(
-            isinstance(s, InpaintingStructure)
-            for s in self.structures.values()
+        structures = (
+            self.structures.values()
+            if isinstance(self.structures, dict)
+            else self.structures.get_structures(strct_type="pymatgen")
         )
+        return all(isinstance(s, InpaintingStructure) for s in structures)
