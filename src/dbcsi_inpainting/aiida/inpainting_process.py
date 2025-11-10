6daff9d186c7d0b4be9f6bbb99c30a5dea469bb1
t-reents <timo.reents1@gmail.com>
2025-11-10 15:36:16 +0100

    MPI parallel inpainting pipeline

--- src/dbcsi_inpainting/aiida/inpainting_process.py
+++ src/dbcsi_inpainting/aiida/inpainting_process.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Any, Dict, List, Tuple
 from torch.utils.data import DataLoader
 from pymatgen.core import Structure
@@ -14,16 +15,18 @@ from mattergen.common.data.transform import (
     symmetrize_lattice,
     set_chemical_system_string,
 )
+import torch
+import warnings
 
 
 DBSCI_INPAINTING_BASE = "dbcsi_inpainting.custom_predictor_corrector"
 GUIDED_PREDICTOR_CORRECTOR_MAPPING = {
     "baseline": "mattergen.diffusion.sampling.classifier_free_guidance.GuidedPredictorCorrector.from_pl_module",
-    "baseline-reverted-order": f"{DBSCI_INPAINTING_BASE}.GuidedPredictorCorrectorRevertedOrder.from_pl_module",
     "baseline-with-noise": f"{DBSCI_INPAINTING_BASE}.CustomGuidedPredictorCorrector.from_pl_module",
+    "baseline-store-scores": f"{DBSCI_INPAINTING_BASE}.AdditionalDataPredictorCorrector.from_pl_module",
     "repaint-v1": f"{DBSCI_INPAINTING_BASE}.CustomGuidedPredictorCorrectorRePaint.from_pl_module",
-    "repaint-v2": f"{DBSCI_INPAINTING_BASE}.CustomGuidedPredictorCorrectorRePaintV2.from_pl_module",
-    "TD": f"{DBSCI_INPAINTING_BASE}.CustomGuidedPredictorCorrectorNewTimesteps.from_pl_module",
+    "repaint-v2": f"{DBSCI_INPAINTING_BASE}.RePaintV2GuidedPredictorCorrector.from_pl_module",
+    "TD": f"{DBSCI_INPAINTING_BASE}.TDPaintGuidedPredictorCorrector.from_pl_module",
 }
 
 
@@ -98,8 +101,14 @@ def _run_inpainting(
         record_trajectories=record_trajectories,
         fix_cell=fix_cell,
     )
-
-    return reconstructed_structures
+    if len(reconstructed_structures) == 2:
+        print("Not returning mean trajectories.")
+        return (reconstructed_structures[0], reconstructed_structures[1], None)
+    elif len(reconstructed_structures) == 3:
+        print("Returning mean trajectories as well.")
+        return reconstructed_structures
+    else:
+        raise ValueError("Unexpected number of outputs from inpainting.")
 
 
 def run_inpainting_pipeline(
@@ -125,7 +134,7 @@ def run_inpainting_pipeline(
         batch_size=config["inpainting_model_params"].get("batch_size", 64),
     )
 
-    inpainted_structures, trajectories = _run_inpainting(
+    inpainted_structures, trajectories, mean_trajectories = _run_inpainting(
         structures_dl=prepared_structures, **config
     )
 
@@ -133,6 +142,11 @@ def run_inpainting_pipeline(
         labels[i]: s for i, s in enumerate(inpainted_structures)
     }
     trajectories = {labels[i]: t for i, t in enumerate(trajectories)}
+
+    if mean_trajectories:
+        mean_trajectories = {
+            labels[i]: t for i, t in enumerate(mean_trajectories)
+        }
 
     outputs = {
         "structures": BatchedStructures(structures=inpainted_structures),
@@ -144,8 +158,130 @@ def run_inpainting_pipeline(
                 "trajectories": trajectories,
             }
         )
+        if mean_trajectories:
+            outputs.update(
+                {
+                    "mean_trajectories": mean_trajectories,
+                }
+            )
+        if Path("recorded_scores.pt").exists():
+            recorded_scores = torch.load("recorded_scores.pt").numpy()
+            atomic_numbers = torch.load("recorded_atomic_numbers.pt").numpy()
+            pos_batch_idx = torch.load("recorded_batch_idx.pt").numpy()
+
+            outputs["scores"] = {
+                "pos_scores": recorded_scores,
+                "atomic_numbers": atomic_numbers,
+                "pos_batch_idx": pos_batch_idx,
+            }
 
     return outputs
+
+
+def run_mpi_parallel_inpainting_pipeline(
+    structures: dict[str, Structure | InpaintingStructure],
+    config: InpaintingPipelineParams | dict[str, Any],
+):
+    """Run the inpainting experiment using MatterGen.
+
+    Args:
+        structures: Input structures for inpainting.
+        config: Configuration for the inpainting process.
+        record_trajectories: Whether to record trajectories during the inpainting process.
+
+    Returns:
+        A dictionary containing inpainted structures, relaxed structures (if applicable), and trajectories.
+    """
+    import mpi4py
+
+    if isinstance(structures, BatchedStructures):
+        structures = structures.get_structures("pymatgen")
+    labels, structures = map(list, zip(*structures.items()))
+
+    comm = mpi4py.MPI.COMM_WORLD
+    rank = comm.rank
+    nranks = comm.size
+    N = len(structures)
+    chunks = np.array_split(np.arange(N), comm.size)
+
+    if torch.cuda.is_available() and torch.cuda.device_count() == nranks:
+        torch.cuda.set_device(rank)
+    elif torch.cuda.is_available():
+        warnings.warn(
+            f"CUDA is available, but the number of GPUs ({torch.cuda.device_count()}) does not match the number of MPI ranks ({nranks})."
+        )
+
+    chunk_idx = chunks[rank]
+    local_structures = [structures[i] for i in chunk_idx]
+    print(f"Rank {rank} processing {len(local_structures)} structures.")
+
+    prepared_structures = __prepare_structures(
+        local_structures,
+        batch_size=config["inpainting_model_params"].get("batch_size", 64),
+    )
+
+    rank_inpainted_structures, rank_trajectories, rank_mean_trajectories = (
+        _run_inpainting(structures_dl=prepared_structures, **config)
+    )
+    results_gathered = comm.gather(
+        (rank_inpainted_structures, rank_trajectories, rank_mean_trajectories),
+        root=0,
+    )
+    if rank == 0:
+        print("Start gathering results from all ranks...")
+        all_inpainted_structures = []
+        all_trajectories = []
+        all_mean_trajectories = []
+        for res in results_gathered:
+            all_inpainted_structures.extend(res[0])
+            all_trajectories.extend(res[1])
+            if res[2]:
+                all_mean_trajectories.extend(res[2])
+
+        print(len(all_mean_trajectories))
+        inpainted_structures = {
+            labels[i]: s for i, s in enumerate(all_inpainted_structures)
+        }
+        trajectories = {labels[i]: t for i, t in enumerate(all_trajectories)}
+        if all_mean_trajectories:
+            mean_trajectories = {
+                labels[i]: t for i, t in enumerate(all_mean_trajectories)
+            }
+
+        outputs = {
+            "structures": BatchedStructures(structures=inpainted_structures),
+        }
+
+        if config["record_trajectories"]:
+            outputs.update(
+                {
+                    "trajectories": trajectories,
+                }
+            )
+            if all_mean_trajectories:
+                print("Returning mean trajectories as well. Updating outputs.")
+                outputs.update(
+                    {
+                        "mean_trajectories": mean_trajectories,
+                    }
+                )
+
+            if Path("recorded_scores.pt").exists():
+                recorded_scores = torch.load("recorded_scores.pt").numpy()
+                atomic_numbers = torch.load(
+                    "recorded_atomic_numbers.pt"
+                ).numpy()
+                pos_batch_idx = torch.load("recorded_batch_idx.pt").numpy()
+
+                outputs["scores"] = {
+                    "pos_scores": recorded_scores,
+                    "atomic_numbers": atomic_numbers,
+                    "pos_batch_idx": pos_batch_idx,
+                }
+        print(outputs.keys())
+        return outputs
+
+    return None
 
 
 def __prepare_structures(
