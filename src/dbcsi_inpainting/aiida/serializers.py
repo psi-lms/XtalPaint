d00556daf1058ea3812ba15e7096864583ba6cff
t-reents <timo.reents1@gmail.com>
2025-11-14 13:27:55 +0100

    Style fixes

--- src/dbcsi_inpainting/aiida/serializers.py
+++ src/dbcsi_inpainting/aiida/serializers.py
@@ -1,18 +1,27 @@
+"""Serializers for converting python datatypes to AiiDA Data nodes."""
+
+from typing import TYPE_CHECKING
+
 from aiida import orm
+
 from dbcsi_inpainting.aiida.data import (
     BatchedStructures,
     BatchedStructuresData,
     PandasDataFrameData,
 )
 
+if TYPE_CHECKING:
+    import pandas as pd
+    from pymatgen.core.structure import Structure
 
-def pymatgen_to_structure_data(structure):
+
+def pymatgen_to_structure_data(structure: Structure) -> orm.StructureData:
+    """Convert a pymatgen Structure to an AiiDA StructureData node."""
     return orm.StructureData(pymatgen=structure)
 
 
 def pymatgen_traj_to_aiida_traj(trajectory):
-    """
-    Convert a pymatgen trajectory to an AiiDA StructureData node.
+    """Convert a pymatgen trajectory to an AiiDA StructureData node.
 
     :param trajectory: A pymatgen trajectory object.
     :return: A list of AiiDA StructureData nodes.
@@ -31,6 +40,6 @@ def batched_structures_to_batched_structures_data(
     return BatchedStructuresData.from_batched_structures(batched_structures)
 
 
-def pandas_dataframe_to_pandas_dataframe_data(df: "pd.DataFrame") -> orm.Data:
+def pandas_dataframe_to_pandas_dataframe_data(df: pd.DataFrame) -> orm.Data:
     """Convert a pandas DataFrame to a custom AiiDA Data node."""
     return PandasDataFrameData(value=df)
